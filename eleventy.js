edbdbcc9f19a0946577ce57a441b6c12a2dfb812
Drum514 <commanderdan@gmail.com>
2026-05-17 15:45:25 -0700

    portrain image added

--- eleventy.js
+++ eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
-  // Copy CSS files to output
+  // Copy CSS files and images to output
   eleventyConfig.addPassthroughCopy({"src/styles.css": "styles.css"});
+  eleventyConfig.addPassthroughCopy({"src/*.jpg": "."});
+  eleventyConfig.addPassthroughCopy({"src/*.png": "."});
   
   return {
     dir: {
